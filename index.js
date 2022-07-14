b1dfad91803e08f7ae8596f06c128531369bec33
marko <belenkov.marko@gmail.com>
2022-07-15 00:09:28 +0300

    Arrays

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ console.log("Hello world!");
         b: 2,
     }
 
+    let someArray = ["red", "green", "blue", 5, true]
+
     console.log(a, b, c+d, !bool, structAlike["a"], structAlike.b)
+    console.log(someArray[0])
 }
 //console.log(a, b) //won`t work because let is enclosed in block
