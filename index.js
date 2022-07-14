5bd9c8b5b1f894e8a797b2e982a2f3be8689ab08
marko <belenkov.marko@gmail.com>
2022-07-15 00:02:53 +0300

    Variable types

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 console.log("Hello world!");
 {
-    var a;
-    let b;
-    console.log(a, b)
+    var a = "Hello";
+    let b = "World!";
+    let c = 5;
+    let d = 5;
+    let bool = false;
+    console.log(a, b, c+d, !bool)
 }
 //console.log(a, b) //won`t work because let is enclosed in block
