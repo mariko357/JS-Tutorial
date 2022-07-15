029d59b848fcb65cd012006c321638981aff17e2
marko <belenkov.marko@gmail.com>
2022-07-15 23:28:59 +0300

    Loops (for, for in, for of, while)

--- index.js
+++ index.js
@@ -50,11 +50,42 @@ console.log("Hello world!");
         return toRet;
     }
 
-    console.log(a, b, c+d, !bool, structAlike["a"], structAlike.b)
-    console.log(someArray[0])
-    printToConsole("Hello")
-    printToConsole(square(53.5))
-    console.log(ifFunc(5), ifFunc(10), ifFunc(50))
-    console.log(switchCase("Monday"), switchCase("Sunday"))
+    function forLoopPrint(count){ //regular for loop
+        for(let i = 0; i < count; i++){
+                console.log(i)
+        }
+    }
+
+    function forLoopIn(input){ // can go through objects (non iterable)
+        for(let i in input){
+                console.log(i)
+        }
+    }
+
+    function forLoopOf(input){ // can go only theough iterable objects
+        for(let i of input){
+                console.log(i)
+        }
+    }
+
+    function whilePrint(count){
+        let i = 0;
+        while(i < count){
+            console.log(i);
+            i++;
+        }
+    }
+
+    console.log(a, b, c+d, !bool, structAlike["a"], structAlike.b);
+    console.log(someArray[0]);
+    printToConsole("Hello");
+    printToConsole(square(53.5));
+    console.log(ifFunc(5), ifFunc(10), ifFunc(50));
+    console.log(switchCase("Monday"), switchCase("Sunday"));
+    forLoopPrint(10);
+    forLoopIn(structAlike); //will print out a and b, not 1 and 2
+    forLoopOf(someArray);
+    whilePrint(10);
+
 }
 //console.log(a, b) //won`t work because let is enclosed in block
