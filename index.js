4a039751385340c11d578f2a39fcb7f59554b3ad
marko <belenkov.marko@gmail.com>
2022-07-15 22:35:30 +0300

    If and switch case

--- index.js
+++ index.js
@@ -21,9 +21,40 @@ console.log("Hello world!");
         return input*input
     }
 
+    function ifFunc(input){
+        if(input === 5){
+            return true
+        }
+        else if(input === 10){
+            return false
+        }
+        else{
+            return "Out of range"
+        }
+    }
+
+    function switchCase(input){
+        let toRet
+        switch (input){
+            case "Monday":
+                toRet = 1;
+                break;
+            case "Tuesday":
+                toRet = 2;
+                break;
+            default:
+                toRet = -1
+                break;
+            
+        }
+        return toRet;
+    }
+
     console.log(a, b, c+d, !bool, structAlike["a"], structAlike.b)
     console.log(someArray[0])
     printToConsole("Hello")
     printToConsole(square(53.5))
+    console.log(ifFunc(5), ifFunc(10), ifFunc(50))
+    console.log(switchCase("Monday"), switchCase("Sunday"))
 }
 //console.log(a, b) //won`t work because let is enclosed in block
